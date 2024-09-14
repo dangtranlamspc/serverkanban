4a0291183f7c9d17e42ba808cba644f1be180b09
dangtranlamspc <dangtranlamspc@gmail.com>
2024-09-14 16:57:55 +0700

    app2

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,36 @@
+import UserModel from "../models/UserModel"
+import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
+dotenv.config();
+
+
 const register = async (req : any, res : any) => {
     const body = req.body
 
+    const {email, name, password } = body
+
     try {
-        console.log(body);
+        
+        const user = await UserModel.findOne({email})
+
+        if (user) {
+            throw new Error('Email đã tồn tại')
+        }
+
+        const salt = await bcrypt.genSalt(10)
+
+        const haspassword = await bcrypt.hash(password, salt)
+
+        body.password = haspassword
+
+        const newUser  = new UserModel(body)
+        await newUser.save();
+
+        // delete newUser.password
+        
         res.status(200).json({
             message : 'Register',
-            data : body
+            data : newUser
         })
     } catch (error : any) {
         res.status(404).json({
