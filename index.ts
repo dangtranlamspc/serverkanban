1623e9047adc33353b65ee3f4823fa7fc97b7281
dangtranlamspc <dangtranlamspc@gmail.com>
2024-09-14 15:15:27 +0700

    run server

--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import userRouter from './src/routers/user'
+import cors from 'cors'
 
 dotenv.config();
 
 const PORT = process.env.FORT || 3001;
 
 const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.lmnpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const app = express();
 
-const app = express()
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/auth', userRouter);
 
 const connectDB = async () => {
     try {
--- src/routers/user.ts
+++ src/routers/user.ts
@@ -3,6 +3,6 @@ import { register } from "../controllers/user";
 
 const router = Router()
 
-    router.post('register', register)
+    router.post('/register', register)
 
 export default router
