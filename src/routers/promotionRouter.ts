013d7e564c464dd3f6a80fe4eceea63d1b1cc03e
dangtranlamspc <tranlam@spchcmc.vn>
2024-12-19 16:58:50 +0700

    fix products

--- src/routers/promotionRouter.ts
+++ src/routers/promotionRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addNew, getPromotion, remove, update } from "../controllers/promotion";
+import { verifyToken } from "../middlewares/verifyToken";
 
 const router = Router();
 
-router.get('/', getPromotion)
+router.get('/', getPromotion);
+router.use(verifyToken);
 router.post('/add-new', addNew);
 router.put('/update', update);
 router.delete('/remove', remove)
